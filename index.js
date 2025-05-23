065e080439d72104c54e5bd9cac19efc37ff20ca
Rifat <mrarifat21@gmail.com>
2025-05-23 18:52:05 +0600

    add delete recipe

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(updated); 
     });
 
+    //  =======Delete a recipe======
+    app.delete("/addrecipes/:id", async(req,res)=>{
+      const id =req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await recipesCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // ====Get recipes for a specific user=====
     app.get("/myrecipes/:email", async (req, res) => {
       const userEmail = req.params.email;
